9c0a6ef4d89252a667813ed695099d1198aef50f
sijun-yang <yangsijun5528@gmail.com>
2025-10-24 11:22:38 +0900

    Revert "just checkpoint"
    This reverts commit a13ae97b7e22978384b62f39d58b450f36a139b5.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,6 @@
-use std::rc::Rc;
-use slint::{SharedString, VecModel, ModelRc};
-
 slint::include_modules!();
 
-fn main() {
-    let ui = ColumnView::new().unwrap();
-
-    // 테스트 데이터
-    let test_data: Vec<ModelRc<SharedString>> = vec![
-        Rc::new(VecModel::from(vec![
-            "Documents".into(),
-            "Downloads".into(),
-            "Pictures".into(),
-            "Music".into(),
-        ])).into(),
-        Rc::new(VecModel::from(vec![
-            "Work".into(),
-            "Personal".into(),
-            "Archive".into(),
-        ])).into(),
-        Rc::new(VecModel::from(vec![
-            "Project1".into(),
-            "Project2".into(),
-            "Project3".into(),
-            "Project4".into(),
-            "Project5".into(),
-        ])).into(),
-    ];
-
-    ui.set_columns(Rc::new(VecModel::from(test_data)).into());
-
-    ui.run().unwrap();
+pub fn main() {
+    let main_window = MainWindow::new().unwrap();
+    main_window.run().unwrap();
 }
