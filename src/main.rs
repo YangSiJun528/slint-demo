d9de29063cc8389e326dc46b2e52a790346aa932
sijun-yang <yangsijun5528@gmail.com>
2025-10-24 18:47:06 +0900

    AI가 짜준거, 스크롤이 안되긴 하는데 디자인은 괜찮

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,6 @@ use slint::{VecModel, ModelRc};
 slint::include_modules!();
 
 fn main() {
-    let ui = ColumnView::new().unwrap();
-
-    let test_data: Vec<ModelRc<RawItem>> = vec![
-        Rc::new(VecModel::from(vec![
-            RawItem { item_type: ItemType::DIRECTORY, name: "Documents".into() },
-            RawItem { item_type: ItemType::DIRECTORY, name: "Downloads".into() },
-            RawItem { item_type: ItemType::DIRECTORY, name: "Downloads".into() },
-            RawItem { item_type: ItemType::DIRECTORY, name: "Downloads".into() },
-            RawItem { item_type: ItemType::DIRECTORY, name: "Downloads".into() },
-            RawItem { item_type: ItemType::DIRECTORY, name: "Downloads".into() },
-            RawItem { item_type: ItemType::FILE, name: "readme.txt".into() },
-        ])).into(),
-        Rc::new(VecModel::from(vec![
-            RawItem { item_type: ItemType::DIRECTORY, name: "Work".into() },
-            RawItem { item_type: ItemType::FILE, name: "report.pdf".into() },
-        ])).into(),
-    ];
-
-    ui.set_column_data(Rc::new(VecModel::from(test_data)).into());
+    let ui = Example::new().unwrap();
     ui.run().unwrap();
 }
